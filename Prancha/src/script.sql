d5431bf626c9cfdf2f058b1e952aa099ffc53d06
Bruna Lima Farias <burunalfarias@gmail.com>
2024-03-26 22:30:49 -0300

    atualização prancha comunicação

---
+++ Prancha/src/script.sql
@@ -0,0 +1,35 @@
+DROP DATABASE IF EXISTS prancha_comunicacao;
+CREATE DATABASE prancha_comunicacao;
+USE prancha_comunicacao;
+
+CREATE TABLE produtos (
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    titulo VARCHAR(45) NOT NULL,
+    preco FLOAT NOT NULL,
+    img TEXT NOT NULL,
+    tipo VARCHAR(25) NOT NULL,
+    descricao VARCHAR(60)
+);
+
+CREATE TABLE carrinho (
+	id_carrinho INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	id_produto INT NOT NULL,
+	quantidade INT,
+	FOREIGN KEY (id_produto) REFERENCES produtos(id)
+);
+
+INSERT INTO produtos(titulo, preco, img, tipo, descricao) VALUES
+	('Uva Thompson', 3.9,  '../img/uva_thompson.jpeg', 'fruta', 'Uva Thompson Perboni Bandeja 500G ');
+
+/*
+SELECT p.titulo, p.preco, p.img, p.quantidade
+FROM produtos p
+INNER JOIN carrinho c ON c.produtos = p.id
+GROUP BY p.id;
+
+CREATE TABLE adm (
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(45) NOT NULL,
+    senha DECIMAL(10, 0) NOT NULL
+);
+*/
