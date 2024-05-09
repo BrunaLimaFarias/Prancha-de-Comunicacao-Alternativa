7c8adbab7419bbf04e3ee2ea8f4b9552c87aadce
Bruna Lima Farias <burunalfarias@gmail.com>
2024-05-09 12:32:29 -0300

    mudança minima banco

--- Prancha/src/js/adiciona-item.js
+++
@@ -1,6 +0,0 @@
-function adicionarItem(item) {
-    var listaFrase = document.getElementById('figura-frase');
-    var novoItem = document.createElement('li');
-    novoItem.textContent = item;
-    listaFrase.appendChild(novoItem);
-}
--- Prancha/src/script.sql
+++ Prancha/src/script.sql
@@ -5,13 +5,13 @@ USE prancha_comunicacao;
 CREATE TABLE lista_figuras (
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     titulo VARCHAR(45) NOT NULL,
-    img TEXT NOT NULL,
-    categoria VARCHAR(30) NOT NULL
+    img TEXT NOT NULL
+    /*categoria VARCHAR(30) NOT NULL*/
 );
 
-INSERT INTO lista_figuras(titulo, img, categoria) VALUES
-	('apontador', '../img/apontador.jpg', 'objeto'),
-    ('arbusto', '../img/arbusto.jpg', 'planta');
+INSERT INTO lista_figuras(titulo, img) VALUES
+	('apontador', '../img/apontador.jpg'),
+    ('arbusto', '../img/arbusto.jpg');
 
 /*
 SELECT p.titulo, p.preco, p.img, p.quantidade
