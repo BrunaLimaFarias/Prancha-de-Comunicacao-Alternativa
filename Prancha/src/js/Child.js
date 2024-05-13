74f3d4c9c8030f58d2136532d23ae9ae3b3b3a8a
Arthur Correia Brígido <51636263+ArthurCorreiaBrigido@users.noreply.github.com>
2024-05-13 19:45:52 -0300

    Delete Prancha/src/js/Child.js

--- Prancha/src/js/Child.js
+++
@@ -1,8 +0,0 @@
-class Child {
-    constructor() {
-        // Inicialização das propriedades de uma instância Child
-        this.Occurrence = 0; // Número de ocorrências da palavra associada ao Child
-        this.Word = ''; // Palavra associada ao Child
-        this.ImagePath = 0; // Caminho da imagem associada à palavra
-    }
-}
